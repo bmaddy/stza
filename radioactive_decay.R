08e1401e44c22497d99b2d1e8f56aaee1d1ee445
Brian Maddy <brian@brianmaddy.com>
2013-07-07 11:43:58 -0500

    changed variable c to cup for clarity

--- radioactive_decay.R
+++ radioactive_decay.R
@@ -5,22 +5,22 @@ make_cup = function(n){
   f
 }
 
-remove = function(c, n){
-  c[c$face != n,]
+remove = function(cup, n){
+  cup[cup$face != n,]
 }
 
-shake = function(c){
-  c$face = sample(1:6, length(c$face), replace = TRUE)
-  c$age = c$age + 1
-  c
+shake = function(cup){
+  cup$face = sample(1:6, length(cup$face), replace = TRUE)
+  cup$age = cup$age + 1
+  cup
 }
 
-experiment = function(c){
-  remaining = c()
-  while(length(c$face) > 0){
-    remaining = append(remaining, length(c$face))
-    c = shake(c)
-    c = remove(c, 1)
+experiment = function(cup){
+  remaining = cup()
+  while(length(cup$face) > 0){
+    remaining = append(remaining, length(cup$face))
+    cup = shake(cup)
+    cup = remove(cup, 1)
   }
-  return(remaining)
+  remaining
 }

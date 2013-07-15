3312c91f63aeeab4223cf24b1f0f5b2057e2412a
Brian Maddy <brian@brianmaddy.com>
2013-07-14 21:35:53 -0500

    overwrote the earthquake code with what we used in class

--- earthquake.R
+++ earthquake.R
@@ -1,41 +1,34 @@
-library("animation")
-
-## Setup
-########
+# Setup
+############
 
 constants = data.frame(breaking_point = 10,
                        distributed_stress = 1)
 
-make_land = function(constants, n = 10){
+make_land = function(n){
   data = sample(0:(constants$breaking_point - 1),
                 n*n,
                 replace = TRUE)
-  land = matrix(data = data, nrow = n, ncol = n)
-  land
+  matrix(data, nrow = n, ncol = n)
 }
 
-## Transition
-#############
+# Transition
+############
 
 add_random_stress = function(land){
-  x = sample(1:ncol(land),1)
-  y = sample(1:nrow(land),1)
-  land[x,y] = land[x,y] + 1
+  x = sample(1:ncol(land), 1)
+  y = sample(1:nrow(land), 1)
+  land[x, y] = land[x, y] + 1
   land
 }
 
-collapse = function(constants, land, x, y){
-  land[x,y] >= constants$breaking_point
-}
-
-distribute_moore = function(constants, land, x, y){
+distribute = function(constants, land, x, y){
   for(dx in -1:1){
     for(dy in -1:1){
-      middle = dx == 0 & dy == 0
-      too_big = x+dx > ncol(land) | y+dy > nrow(land)
+      center = dx == 0 & dy == 0
       too_small = x+dx < 1 | y+dy < 1
-      if(!middle & !too_big & !too_small){
-        land[x+dx,y+dy] = land[x+dx,y+dy] + constants$distributed_stress
+      too_big = x+dx > ncol(land) | y+dy > nrow(land)
+      if(!center & !too_big & !too_small){
+        land[x+dx, y+dy] = land[x+dx, y+dy] + constants$distributed_stress
       }
     }
   }
@@ -47,20 +40,23 @@ collapse_and_distribute_all = function(constants, land){
   collapse_count = 0
   for(x in 1:ncol(land)){
     for(y in 1:nrow(land)){
-      if(collapse(constants, land,x,y)){
+      if(land[x, y] >= constants$breaking_point){
+        result[x, y] = land[x, y] - constants$breaking_point
         collapse_count = collapse_count + 1
-        result[x,y] = land[x,y] - constants$breaking_point
-        result = distribute_moore(constants, result, x, y)
+        result = distribute(constants, result, x, y)
       }
     }
   }
   list(land = result, collapse_count = collapse_count)
 }
 
+# Iterate
+############
 
-## Iterate
-##########
-
+# calls collapse_and_distribute_all repatedly until there are no
+# at the breaking_point
+# returns: the new version of land and the total number of collapses
+# that happened (collapse_count)
 propagate = function(constants, land){
   collapse_count = 0
   while(max(land) > constants$breaking_point){
@@ -71,30 +67,39 @@ propagate = function(constants, land){
   list(land = land, collapse_count = collapse_count)
 }
 
-stress_and_propagate = function(constants, land, n, make_plot = FALSE){
+# This will iterate a total of n times. Each time it will add some
+# random stress and propagate any collapses that happen.
+# returns: a vector of collapses, one for each iteration n
+add_stress_and_propagate = function(constants, land, n){
   collapses = c()
   for(i in 1:n){
     land = add_random_stress(land)
     propagated = propagate(constants, land)
     land = propagated$land
     collapses = append(collapses, propagated$collapse_count)
-    if(make_plot){plot_state(land)}
+    plot_state(land)
   }
   list(land = land, collapses = collapses)
 }
 
-## Report
-#########
+# Report
+############
 
+# makes a new blank plot and then draws the current state of land
+# using rectangles. You'll need to add a call to this in the loop in
+# add_stress_and_propagate
 plot_state = function(land){
-  plot(1,1,type = "n", xlim = c(0,ncol(land)), ylim = c(0,nrow(land)))
+  plot(1, 1, type = "n", xlim = c(0, ncol(land)), ylim = c(0, nrow(land)))
   for(x in 1:ncol(land)){
     for(y in 1:nrow(land)){
-      rect(x-1, y-1, x, y, col = terrain.colors(10)[land[x,y]])
+      #rect(x-1, y-1, x, y, col = terrain.colors(10)[land[x,y]])
+      rect(x-1, y-1, x, y, col = 'darkseagreen', density = 5*land[x,y])
     }
   }
 }
 
+# Starts an animation and calls add_stress_and_propagate to
+# draw the plots
 plot_state_over_time = function(constants, land, n){
   ani.options(interval = 0.1)
   ani.start()
@@ -102,7 +107,3 @@ plot_state_over_time = function(constants, land, n){
   ani.stop()
   result
 }
-
-# land = make_land(constants, 50)
-# result = plot_state_over_time(land, 500)
-# plot_state_over_time(density(result$collapses))
